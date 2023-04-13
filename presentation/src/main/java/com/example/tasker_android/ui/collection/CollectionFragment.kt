6c522c75758a4710f0a9792391df3b3061f20c79
dongk00 <dddongk00@gmail.com>
2023-04-13 20:03:35 +0900

    BaseFragment 생성

--- presentation/src/main/java/com/example/tasker_android/ui/collection/CollectionFragment.kt
+++ presentation/src/main/java/com/example/tasker_android/ui/collection/CollectionFragment.kt
@@ -1,11 +1,7 @@
 package com.example.tasker_android.ui.collection
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.example.tasker_android.R
 import com.example.tasker_android.base.BaseFragment
 import com.example.tasker_android.databinding.FragmentCollectionBinding
 
--- presentation/src/main/java/com/example/tasker_android/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/example/tasker_android/ui/home/HomeFragment.kt
@@ -1,13 +1,8 @@
 package com.example.tasker_android.ui.home
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.example.tasker_android.R
 import com.example.tasker_android.base.BaseFragment
-import com.example.tasker_android.databinding.FragmentCollectionBinding
 import com.example.tasker_android.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
