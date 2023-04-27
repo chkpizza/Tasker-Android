1ba0cef1fcfb8a8c3b0444e7b807a2add62b910b
chkpizza <48915641+chkpizza@users.noreply.github.com>
2023-04-28 00:24:53 +0900

    add app navigation graph

--- app/src/main/java/com/example/tasker_android/MainActivity.kt
+++ app/src/main/java/com/example/tasker_android/MainActivity.kt
@@ -2,22 +2,14 @@ package com.example.tasker_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupWithNavController
+import androidx.databinding.DataBindingUtil
 import com.example.tasker_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
